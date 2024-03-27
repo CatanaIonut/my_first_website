04886a633a1f6562c79ca45ad7241c59422ccb31
CatanaIonut <catanaionut79@gmail.com>
2024-03-27 16:57:17 +0200

    add-nume

--- my_first_website/src/App.tsx
+++ my_first_website/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Catana Ionut</h1>
+      <h1>tatamare123</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
